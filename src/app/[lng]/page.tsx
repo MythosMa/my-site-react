f1f16dbd65254689a5355b0d2b3e547ce9b55d5a
MYTHOSMA-PC\马原 <mythosmayuan@gmail.com>
2024-03-07 18:10:38 +0800

    update

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,7 +1,13 @@
 const Home = () => {
   return (
     <div className="flex flex-col w-full">
-      <div className="px-8">1111</div>
+      <div className="px-8 w-full flex flex-row bg-red-500">
+        <div className="w-2/3 h-full flex flex-col bg-blue-500">
+          <div className="w-full h-1/2 bg-yellow-500">123123</div>
+          <div className="w-full h-1/2 bg-gray-500">123123</div>
+        </div>
+        <div className="w-1/3 h-full bg-green-500">123123</div>
+      </div>
       <div>1111</div>
       <div>1111</div>
       <div>1111</div>
