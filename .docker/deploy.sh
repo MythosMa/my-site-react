385420dceaed4faba43f73ac35483f7d440f2888
MythosMa <mythosmayuan@gmail.com>
2025-02-18 16:07:09 +0800

    update docker images

--- .docker/deploy.sh
+++ .docker/deploy.sh
@@ -1,4 +1,4 @@
-export BUILD_IMG_TAG=1.3.0
+export BUILD_IMG_TAG=1.3.1
 
 sh .docker/check-docker.sh
 
@@ -9,8 +9,8 @@ cp .docker/Dockerfile ./
 
 docker buildx build \
    --platform=linux/amd64 \
-   --tag mythosma/my-site:$BUILD_IMG_TAG \
-   --push -t mythosma/my-site:$BUILD_IMG_TAG .
+   --tag ccr.ccs.tencentyun.com/mythosma/my-site:$BUILD_IMG_TAG \
+   --push -t ccr.ccs.tencentyun.com/mythosma/my-site:$BUILD_IMG_TAG .
 
 # docker tag my-site mythosma/my-site:$BUILD_IMG_TAG
 # docker push mythosma/my-site:$BUILD_IMG_TAG
